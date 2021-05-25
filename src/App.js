cb6829df346adf287b0bc20624c19c515837e16e
beatriznonato <bia_nonato@icloud.com>
2021-05-25 13:52:45 -0300

    style: add style header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/Home/Home'
+import './global.css'
 
 function App() {
   return <Home />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <header>
-                <img src="https://courier-images-web.imgix.net/static/img/white-logo.png?auto=compress,format&fit=max&w=undefined&h=undefined&dpr=2&fm=png"/>
-                <button>Entrar</button>
-            </header>
+        <div className="home__container">
+                <header className="home__header">
+                    <img className="home__logo" src="https://courier-images-web.imgix.net/static/img/white-logo.png?auto=compress,format&fit=max&w=undefined&h=undefined&dpr=2&fm=png"/>
+                    <button className="home__button home__button--tex">ENTRAR</button>
+                </header>
             <main>
-                <h1>Bebidas geladas a preço de mercado na sua casa agora</h1>
+                <h1 className="home__title">Bebidas geladas a preço de mercado na sua casa agora</h1>
             </main>
         </div>
     )
