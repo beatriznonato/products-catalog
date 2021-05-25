268de35d8be34ec9acb69cfed25193a102360f10
beatriznonato <bia_nonato@icloud.com>
2021-05-25 11:08:45 -0300

    feat: create page Home and html structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
+import Home from './pages/Home/Home'
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
-  )
+  return <Home />
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <header>
+                <img src="https://courier-images-web.imgix.net/static/img/white-logo.png?auto=compress,format&fit=max&w=undefined&h=undefined&dpr=2&fm=png"/>
+                <button>Entrar</button>
+            </header>
+            <main>
+                <h1>Bebidas geladas a preço de mercado na sua casa agora</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Home;
