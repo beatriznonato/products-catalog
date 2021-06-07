3183e04dd231fd4a5bc68b1d8a7ace33f502748f
beatriznonato <bia_nonato@icloud.com>
2021-06-07 13:12:46 -0300

    feat: create page products

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Products from './pages/Products/Products'
 
 ReactDOM.render(
     <App />,
+    <Products />,
     document.getElementById("root")
 );
---
+++ src/pages/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+function Products() {
+    return (
+        <div>
+            <Header />
+            <main></main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Products;
