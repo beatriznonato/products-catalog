5ae5a192f010f443b9613f68310e912f631ae3d1
beatriznonato <bia_nonato@icloud.com>
2021-06-07 14:20:58 -0300

    feat: add route between pages

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,28 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom'
 import location from '../../assets/image/location.png'
 import './Input.css'
 
 function Input() {
     const [address, setAddress] = useState('')
 
+    const googleMaps = async () => {
+        const resultGoogleMaps = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=AIzaSyBqPblsbU-npXGGomuXMC21oR4kb13-K6E");
+        const {data} = resultGoogleMaps
+        // console.log(data)
+
+        if(data.status == "OK"){
+            await zeApi(data.results[0].geometry.location)
+        } 
+        // console.log(resultGoogleMaps)
+    }
+
     const zeApi = async () => {
         const resultZe = await axios.post("https://api.code-challenge.ze.delivery/public/graphql", {
             query: `
             query pocSearchMethod($now: DateTime!, $algorithm: String!, $lat: String!, $long: String!) {
                 pocSearch(now: $now, algorithm: $algorithm, lat: $lat, long: $long) {
-                  address {
-                    __typename
-                    address1
-                    address2
-                    number
-                    city
-                    province
-                    zip
-                    coordinates
-                  }
+                    address {
+                        __typename
+                        address1
+                        address2
+                        number
+                        city
+                        province
+                        zip
+                        coordinates
+                    }
                 }
-              }
+            }
             `,
             variables: {
                 algorithm: "NEAREST",
@@ -35,23 +47,15 @@ function Input() {
         })
     } 
     
-    const googleMaps = async () => {
-        const resultGoogleMaps = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=AIzaSyBqPblsbU-npXGGomuXMC21oR4kb13-K6E");
-        const {data} = resultGoogleMaps
-        // console.log(data)
-
-        if(data.status == "OK"){
-            await zeApi(data.results[0].geometry.location)
-        } 
-        // console.log(resultGoogleMaps)
-    }
-
     const handleKeyDown = async (event) => {
         if (event.charCode == 13 || event.key == "Enter") {
             await googleMaps();
+            history.push("/produtos")
         }
     }
 
+    const history = useHistory();
+    
     return (
         <div className="input">
             <input 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Products from './pages/Products/Products'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
-    <App />,
-    <Products />,
-    document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/produtos" component={Products} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById("root")
 );
